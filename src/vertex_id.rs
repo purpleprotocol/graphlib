9ff5a8b6153a9e8a3aae6accb85c67cb60bef626
Octavian Oncescu <octavian.oncescu@gmail.com>
2019-03-16 11:07:50 +0100

    Changed vertex id size to 128bits

--- src/vertex_id.rs
+++ src/vertex_id.rs
@@ -3,11 +3,11 @@
 use rand::Rng;
 
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
-pub struct VertexId([u8; 12]);
+pub struct VertexId([u8; 16]); // 128bit 
 
 impl VertexId {
     pub fn random() -> VertexId {
-        let bytes = rand::thread_rng().gen::<[u8; 12]>();
+        let bytes = rand::thread_rng().gen::<[u8; 16]>();
         VertexId(bytes)
     }
 }
