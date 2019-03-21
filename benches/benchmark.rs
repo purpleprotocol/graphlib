b68f5553b6a3221fd95b7eaad1f0e952512007a4
sn99 <siddharthn.099@gmail.com>
2019-03-21 23:05:35 +0530

    refractor benchmarks

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -33,34 +33,31 @@ fn bench_create(c: &mut Criterion) {
 // 16.vertex_count(&self) -> usize
 fn bench_others(c: &mut Criterion) {
     c.bench_function("add_edge", |b| {
+        let mut graph: Graph<usize> = Graph::new();
+
+        let v1 = graph.add_vertex(1);
+        let v2 = graph.add_vertex(2);
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
-
-            let v1 = graph.add_vertex(1);
-            let v2 = graph.add_vertex(2);
-
-            graph.add_edge(&v1, &v2);
-            graph.add_edge(&v1, &v2);
             graph.add_edge(&v1, &v2);
         })
     });
     c.bench_function("add_vertex", |b| {
+        let mut graph: Graph<usize> = Graph::new();
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
             let _id = graph.add_vertex(1);
         })
     });
+
     c.bench_function("capacity", |b| {
         b.iter(|| {
             let graph: Graph<usize> = Graph::with_capacity(5);
-
             let _k = graph.capacity();
         })
     });
-    c.bench_function("edge_count", |b| {
-        b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
 
+    c.bench_function("edge_count", |b| {
+        let mut graph: Graph<usize> = Graph::new();
+        b.iter(|| {
             let v1 = graph.add_vertex(0);
             let v2 = graph.add_vertex(1);
             let v3 = graph.add_vertex(2);
@@ -69,160 +66,143 @@ fn bench_others(c: &mut Criterion) {
             graph.add_edge(&v1, &v2).unwrap();
             graph.add_edge(&v2, &v3).unwrap();
             graph.add_edge(&v3, &v4).unwrap();
-
             let _k = graph.edge_count();
         })
     });
     c.bench_function("fetch", |b| {
+        let mut graph: Graph<usize> = Graph::new();
+        let id = graph.add_vertex(1);
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
-            let id = graph.add_vertex(1);
-
             let _k = *graph.fetch(&id).unwrap();
         })
     });
     c.bench_function("fetch_mut", |b| {
+        let mut graph: Graph<usize> = Graph::new();
+        let id = graph.add_vertex(1);
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
-            let id = graph.add_vertex(1);
-
             let _v = graph.fetch_mut(&id).unwrap();
         })
     });
     c.bench_function("fold", |b| {
+        let mut graph: Graph<usize> = Graph::new();
+
+        graph.add_vertex(1);
+        graph.add_vertex(2);
+        graph.add_vertex(3);
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
-
-            graph.add_vertex(1);
-            graph.add_vertex(2);
-            graph.add_vertex(3);
-
             let _result = graph.fold(0, |v, acc| v + acc);
         })
     });
     c.bench_function("has_edge", |b| {
+        let mut graph: Graph<usize> = Graph::new();
+
+        let v1 = graph.add_vertex(1);
+        let v2 = graph.add_vertex(2);
+        let v3 = graph.add_vertex(3);
+
+        graph.add_edge(&v1, &v2).unwrap();
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
-
-            let v1 = graph.add_vertex(1);
-            let v2 = graph.add_vertex(2);
-            let v3 = graph.add_vertex(3);
-
-            graph.add_edge(&v1, &v2).unwrap();
-
-            assert!(graph.has_edge(&v1, &v2));
-            assert!(!graph.has_edge(&v2, &v3));
+            let _k = graph.has_edge(&v1, &v2);
+            let _l = graph.has_edge(&v2, &v3);
         })
     });
     c.bench_function("is_cyclic", |b| {
+        let mut graph: Graph<usize> = Graph::new();
+
+        let v1 = graph.add_vertex(0);
+        let v2 = graph.add_vertex(1);
+        let v3 = graph.add_vertex(2);
+        let v4 = graph.add_vertex(3);
+
+        graph.add_edge(&v1, &v2).unwrap();
+        graph.add_edge(&v2, &v3).unwrap();
+        graph.add_edge(&v3, &v4).unwrap();
+
+        graph.add_edge(&v3, &v1);
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
-
-            let v1 = graph.add_vertex(0);
-            let v2 = graph.add_vertex(1);
-            let v3 = graph.add_vertex(2);
-            let v4 = graph.add_vertex(3);
-
-            graph.add_edge(&v1, &v2).unwrap();
-            graph.add_edge(&v2, &v3).unwrap();
-            graph.add_edge(&v3, &v4).unwrap();
-
-            let a = graph.is_cyclic();
-
-            graph.add_edge(&v3, &v1);
-
-            let k = graph.is_cyclic();
+            let _k = graph.is_cyclic();
         })
     });
     c.bench_function("remove", |b| {
+        let mut graph: Graph<usize> = Graph::new();
+
+        let v1 = graph.add_vertex(1);
+        let v2 = graph.add_vertex(2);
+        let v3 = graph.add_vertex(3);
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
-
-            let v1 = graph.add_vertex(1);
-            let v2 = graph.add_vertex(2);
-            let v3 = graph.add_vertex(3);
-
             graph.remove(&v2);
             graph.remove(&v1);
             graph.remove(&v3);
         })
     });
     c.bench_function("remove_edge", |b| {
+        let mut graph: Graph<usize> = Graph::new();
+
+        let v1 = graph.add_vertex(0);
+        let v2 = graph.add_vertex(1);
+        let v3 = graph.add_vertex(2);
+        let v4 = graph.add_vertex(3);
+
+        graph.add_edge(&v1, &v2).unwrap();
+        graph.add_edge(&v2, &v3).unwrap();
+        graph.add_edge(&v3, &v4).unwrap();
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
-
-            let v1 = graph.add_vertex(0);
-            let v2 = graph.add_vertex(1);
-            let v3 = graph.add_vertex(2);
-            let v4 = graph.add_vertex(3);
-
-            graph.add_edge(&v1, &v2).unwrap();
-            graph.add_edge(&v2, &v3).unwrap();
-            graph.add_edge(&v3, &v4).unwrap();
-
-            assert_eq!(graph.edge_count(), 3);
-
             graph.remove_edge(&v2, &v3);
             graph.remove_edge(&v2, &v3);
             graph.remove_edge(&v2, &v3);
         })
     });
     c.bench_function("reserve", |b| {
+        let mut graph: Graph<usize> = Graph::with_capacity(3);
+
+        graph.add_vertex(0);
+        graph.add_vertex(1);
+        graph.add_vertex(2);
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::with_capacity(3);
-
-            graph.add_vertex(0);
-            graph.add_vertex(1);
-            graph.add_vertex(2);
-
             graph.reserve(10);
         })
     });
     c.bench_function("retain", |b| {
+        let mut graph: Graph<usize> = Graph::new();
+
+        graph.add_vertex(1);
+        graph.add_vertex(2);
+        graph.add_vertex(2);
+        graph.add_vertex(2);
+        graph.add_vertex(3);
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
-
-            graph.add_vertex(1);
-            graph.add_vertex(2);
-            graph.add_vertex(2);
-            graph.add_vertex(2);
-            graph.add_vertex(3);
-
             graph.retain(|v| *v != 2);
         })
     });
     c.bench_function("roots_count", |b| {
+        let mut graph: Graph<usize> = Graph::new();
+        let v1 = graph.add_vertex(0);
+        let v2 = graph.add_vertex(1);
+        let v3 = graph.add_vertex(2);
+        let v4 = graph.add_vertex(3);
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
-
-            let v1 = graph.add_vertex(0);
-            let v2 = graph.add_vertex(1);
-            let v3 = graph.add_vertex(2);
-            let v4 = graph.add_vertex(3);
-
             graph.add_edge(&v1, &v2).unwrap();
             graph.add_edge(&v3, &v1).unwrap();
             graph.add_edge(&v1, &v4).unwrap();
 
-            assert_eq!(graph.roots_count(), 1);
+            let _k = graph.roots_count();
         })
     });
-    c.bench_function("shrink_to_fit", |b| {
-        b.iter(|| {
-            let mut graph: Graph<usize> = Graph::with_capacity(5);
 
+    c.bench_function("shrink_to_fit", |b| {
+        let mut graph: Graph<usize> = Graph::with_capacity(5);
+
+        b.iter(|| {
             graph.shrink_to_fit();
         })
     });
     c.bench_function("vertex_count", |b| {
+        let mut graph: Graph<usize> = Graph::new();
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
-
             graph.add_vertex(1);
             graph.add_vertex(2);
             graph.add_vertex(3);
-
-            assert_eq!(graph.vertex_count(), 3);
+            let _k = graph.vertex_count();
         })
     });
 }
@@ -234,19 +214,18 @@ fn bench_others(c: &mut Criterion) {
 // 4. roots(&self) -> VertexIter
 fn bench_iterators(c: &mut Criterion) {
     c.bench_function("dfs", |b| {
+        let mut graph: Graph<usize> = Graph::new();
+        let mut vertices = vec![];
+
+        let v1 = graph.add_vertex(0);
+        let v2 = graph.add_vertex(1);
+        let v3 = graph.add_vertex(2);
+        let v4 = graph.add_vertex(3);
+
+        graph.add_edge(&v1, &v2).unwrap();
+        graph.add_edge(&v3, &v1).unwrap();
+        graph.add_edge(&v1, &v4).unwrap();
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
-            let mut vertices = vec![];
-
-            let v1 = graph.add_vertex(0);
-            let v2 = graph.add_vertex(1);
-            let v3 = graph.add_vertex(2);
-            let v4 = graph.add_vertex(3);
-
-            graph.add_edge(&v1, &v2).unwrap();
-            graph.add_edge(&v3, &v1).unwrap();
-            graph.add_edge(&v1, &v4).unwrap();
-
             for v in graph.dfs() {
                 vertices.push(v);
             }
@@ -254,25 +233,24 @@ fn bench_iterators(c: &mut Criterion) {
     });
 
     c.bench_function("bfs", |b| {
+        let mut graph: Graph<usize> = Graph::new();
+        let mut vertices = vec![];
+
+        let v1 = graph.add_vertex(0);
+        let v2 = graph.add_vertex(1);
+        let v3 = graph.add_vertex(2);
+        let v4 = graph.add_vertex(3);
+        let v5 = graph.add_vertex(4);
+        let v6 = graph.add_vertex(5);
+        let v7 = graph.add_vertex(6);
+
+        graph.add_edge(&v1, &v2).unwrap();
+        graph.add_edge(&v3, &v1).unwrap();
+        graph.add_edge(&v1, &v4).unwrap();
+        graph.add_edge(&v1, &v7).unwrap();
+        graph.add_edge(&v2, &v5).unwrap();
+        graph.add_edge(&v5, &v6).unwrap();
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
-            let mut vertices = vec![];
-
-            let v1 = graph.add_vertex(0);
-            let v2 = graph.add_vertex(1);
-            let v3 = graph.add_vertex(2);
-            let v4 = graph.add_vertex(3);
-            let v5 = graph.add_vertex(4);
-            let v6 = graph.add_vertex(5);
-            let v7 = graph.add_vertex(6);
-
-            graph.add_edge(&v1, &v2).unwrap();
-            graph.add_edge(&v3, &v1).unwrap();
-            graph.add_edge(&v1, &v4).unwrap();
-            graph.add_edge(&v1, &v7).unwrap();
-            graph.add_edge(&v2, &v5).unwrap();
-            graph.add_edge(&v5, &v6).unwrap();
-
             for v in graph.bfs() {
                 vertices.push(v);
             }
@@ -280,15 +258,14 @@ fn bench_iterators(c: &mut Criterion) {
     });
 
     c.bench_function("vertices", |b| {
+        let mut graph: Graph<usize> = Graph::new();
+        let mut vertices = vec![];
+
+        let _v1 = graph.add_vertex(0);
+        let _v2 = graph.add_vertex(1);
+        let _v3 = graph.add_vertex(2);
+        let _v4 = graph.add_vertex(3);
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
-            let mut vertices = vec![];
-
-            let _v1 = graph.add_vertex(0);
-            let _v2 = graph.add_vertex(1);
-            let _v3 = graph.add_vertex(2);
-            let _v4 = graph.add_vertex(3);
-
             for v in graph.vertices() {
                 vertices.push(v);
             }
@@ -296,19 +273,18 @@ fn bench_iterators(c: &mut Criterion) {
     });
 
     c.bench_function("roots", |b| {
+        let mut graph: Graph<usize> = Graph::new();
+        let mut roots = vec![];
+
+        let v1 = graph.add_vertex(0);
+        let v2 = graph.add_vertex(1);
+        let v3 = graph.add_vertex(2);
+        let v4 = graph.add_vertex(3);
+
+        graph.add_edge(&v1, &v2).unwrap();
+        graph.add_edge(&v3, &v1).unwrap();
+        graph.add_edge(&v1, &v4).unwrap();
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
-            let mut roots = vec![];
-
-            let v1 = graph.add_vertex(0);
-            let v2 = graph.add_vertex(1);
-            let v3 = graph.add_vertex(2);
-            let v4 = graph.add_vertex(3);
-
-            graph.add_edge(&v1, &v2).unwrap();
-            graph.add_edge(&v3, &v1).unwrap();
-            graph.add_edge(&v1, &v4).unwrap();
-
             for v in graph.roots() {
                 roots.push(v);
             }
@@ -325,104 +301,103 @@ fn bench_iterators(c: &mut Criterion) {
 // 6. out_neighbors_count(&self, id: &VertexId) -> usize
 fn bench_neighbor_functions(c: &mut Criterion) {
     c.bench_function("neighbors_count", |b| {
+        let mut graph: Graph<usize> = Graph::new();
+
+        let v1 = graph.add_vertex(0);
+        let v2 = graph.add_vertex(1);
+        let v3 = graph.add_vertex(2);
+        let v4 = graph.add_vertex(3);
+
+        graph.add_edge(&v1, &v2).unwrap();
+        graph.add_edge(&v3, &v1).unwrap();
+        graph.add_edge(&v1, &v4).unwrap();
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
-
-            let v1 = graph.add_vertex(0);
-            let v2 = graph.add_vertex(1);
-            let v3 = graph.add_vertex(2);
-            let v4 = graph.add_vertex(3);
-
-            graph.add_edge(&v1, &v2).unwrap();
-            graph.add_edge(&v3, &v1).unwrap();
-            graph.add_edge(&v1, &v4).unwrap();
+            let _k = graph.neighbors_count(&v1);
         })
     });
     c.bench_function("in_neighbors_count", |b| {
+        let mut graph: Graph<usize> = Graph::new();
+
+        let v1 = graph.add_vertex(0);
+        let v2 = graph.add_vertex(1);
+        let v3 = graph.add_vertex(2);
+        let v4 = graph.add_vertex(3);
+
+        graph.add_edge(&v1, &v2).unwrap();
+        graph.add_edge(&v3, &v1).unwrap();
+        graph.add_edge(&v1, &v4).unwrap();
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
-
-            let v1 = graph.add_vertex(0);
-            let v2 = graph.add_vertex(1);
-            let v3 = graph.add_vertex(2);
-            let v4 = graph.add_vertex(3);
-
-            graph.add_edge(&v1, &v2).unwrap();
-            graph.add_edge(&v3, &v1).unwrap();
-            graph.add_edge(&v1, &v4).unwrap();
+            let _k = graph.in_neighbors_count(&v1);
         })
     });
     c.bench_function("out_neighbors_count", |b| {
+        let mut graph: Graph<usize> = Graph::new();
+
+        let v1 = graph.add_vertex(0);
+        let v2 = graph.add_vertex(1);
+        let v3 = graph.add_vertex(2);
+        let v4 = graph.add_vertex(3);
+        let v5 = graph.add_vertex(4);
+
+        graph.add_edge(&v1, &v2).unwrap();
+        graph.add_edge(&v3, &v1).unwrap();
+        graph.add_edge(&v1, &v4).unwrap();
+        graph.add_edge(&v2, &v5).unwrap();
+        graph.add_edge(&v2, &v3).unwrap();
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
-
-            let v1 = graph.add_vertex(0);
-            let v2 = graph.add_vertex(1);
-            let v3 = graph.add_vertex(2);
-            let v4 = graph.add_vertex(3);
-            let v5 = graph.add_vertex(4);
-
-            graph.add_edge(&v1, &v2).unwrap();
-            graph.add_edge(&v3, &v1).unwrap();
-            graph.add_edge(&v1, &v4).unwrap();
-            graph.add_edge(&v2, &v5).unwrap();
-            graph.add_edge(&v2, &v3).unwrap();
+            let _k = graph.out_neighbors_count(&v1);
+            let _l = graph.out_neighbors_count(&v2);
         })
     });
     c.bench_function("in_neighbors", |b| {
+        let mut graph: Graph<usize> = Graph::new();
+        let mut neighbors = vec![];
+
+        let v1 = graph.add_vertex(0);
+        let v2 = graph.add_vertex(1);
+        let v3 = graph.add_vertex(2);
+        let v4 = graph.add_vertex(3);
+
+        graph.add_edge(&v1, &v2).unwrap();
+        graph.add_edge(&v3, &v1).unwrap();
+        graph.add_edge(&v1, &v4).unwrap();
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
-            let mut neighbors = vec![];
-
-            let v1 = graph.add_vertex(0);
-            let v2 = graph.add_vertex(1);
-            let v3 = graph.add_vertex(2);
-            let v4 = graph.add_vertex(3);
-
-            graph.add_edge(&v1, &v2).unwrap();
-            graph.add_edge(&v3, &v1).unwrap();
-            graph.add_edge(&v1, &v4).unwrap();
-
             for v in graph.in_neighbors(&v1) {
                 neighbors.push(v);
             }
         })
     });
     c.bench_function("out_neighbors", |b| {
+        let mut graph: Graph<usize> = Graph::new();
+        let mut neighbors = vec![];
+
+        let v1 = graph.add_vertex(0);
+        let v2 = graph.add_vertex(1);
+        let v3 = graph.add_vertex(2);
+        let v4 = graph.add_vertex(3);
+
+        graph.add_edge(&v1, &v2).unwrap();
+        graph.add_edge(&v3, &v1).unwrap();
+        graph.add_edge(&v1, &v4).unwrap();
         b.iter(|| {
-            use graphlib::Graph;
-
-            let mut graph: Graph<usize> = Graph::new();
-            let mut neighbors = vec![];
-
-            let v1 = graph.add_vertex(0);
-            let v2 = graph.add_vertex(1);
-            let v3 = graph.add_vertex(2);
-            let v4 = graph.add_vertex(3);
-
-            graph.add_edge(&v1, &v2).unwrap();
-            graph.add_edge(&v3, &v1).unwrap();
-            graph.add_edge(&v1, &v4).unwrap();
-
             for v in graph.out_neighbors(&v1) {
                 neighbors.push(v);
             }
         })
     });
     c.bench_function("neighbors", |b| {
+        let mut graph: Graph<usize> = Graph::new();
+        let mut neighbors = vec![];
+
+        let v1 = graph.add_vertex(0);
+        let v2 = graph.add_vertex(1);
+        let v3 = graph.add_vertex(2);
+        let v4 = graph.add_vertex(3);
+
+        graph.add_edge(&v1, &v2).unwrap();
+        graph.add_edge(&v3, &v1).unwrap();
+        graph.add_edge(&v1, &v4).unwrap();
         b.iter(|| {
-            let mut graph: Graph<usize> = Graph::new();
-            let mut neighbors = vec![];
-
-            let v1 = graph.add_vertex(0);
-            let v2 = graph.add_vertex(1);
-            let v3 = graph.add_vertex(2);
-            let v4 = graph.add_vertex(3);
-
-            graph.add_edge(&v1, &v2).unwrap();
-            graph.add_edge(&v3, &v1).unwrap();
-            graph.add_edge(&v1, &v4).unwrap();
-
             for v in graph.neighbors(&v1) {
                 neighbors.push(v);
             }
